9a0114e39d0b6733a84a3df3e54e4d9702720eb2
unknown <anacarol_1808@hotmail.com>
2023-02-27 13:56:55 -0700

    My finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+
+        // Getting First Name
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        // Getting Last Name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Printing it to the terminal
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
+
+
     }
 }
