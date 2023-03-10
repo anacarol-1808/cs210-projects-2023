3a37f096d32278115f9ec4361983ac71c00ac9be
unknown <anacarol_1808@hotmail.com>
2023-03-10 14:21:17 -0700

    My finished Learning Activity 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // create an instance for 1/1 (using the first constructor)
+        var fraction = new Fraction();
+        fraction.GetFraction();
+
+        // for 6/1 (using the second constructor)
+        var fraction1 = new Fraction(6);
+        fraction1.GetFraction();
+
+        // for 6/7 (using the third constructor)
+        var fraction2 = new Fraction(6 , 7);
+        fraction2.GetFraction();
+
+        fraction.SetTop(8);
+        Console.WriteLine(fraction.GetTop());
+
+        fraction.SetBottom(10);
+        fraction.GetFraction();
+        Console.WriteLine(fraction.GetBottom());
+
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
