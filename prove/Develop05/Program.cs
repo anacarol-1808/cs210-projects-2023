a58c521d660af01aa85d0eca8a06c1c18b8f5bef
unknown <anacarol_1808@hotmail.com>
2023-03-24 21:36:56 -0600

    my best version for Friday night

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,10 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        UserInterface userInterface = new UserInterface();
+        int _totalPoints = 0;
+        string _menuOption = "0";
+        Goal goal = new Goal();
 
-        userInterface.DisplayMenu();
-    
-        
+        while (_menuOption != "6")
+        { 
+            Console.WriteLine($"\r\nYou have {_totalPoints} points.\r\n");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1. Create New Goal");
+            Console.WriteLine(" 2. List Goals");
+            Console.WriteLine(" 3. Save Goals");
+            Console.WriteLine(" 4. Load Goals");
+            Console.WriteLine(" 5. Record Event");
+            Console.WriteLine(" 6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            _menuOption = Console.ReadLine();
+
+            if (_menuOption == "1")
+            {
+                goal.CreateGoal();
+            }
+        }
+            
     }
 }
